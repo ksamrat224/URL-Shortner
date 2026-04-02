2873f5af4b86d2f94dce85150972f6520bc7ee7a
Samrat <ksamrat224@gmail.com>
2026-04-02 21:02:46 +0545

    feat: add URL shortener UI to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black p-4">
+      <h1 className="text-4xl font-bold text-center">URL Shortner</h1>
+      <input
+        type="text"
+        className="w-full p-4 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        placeholder="Enter your url"
+      />
+      <button className="mt-4 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">
+        Shorten URL
+      </button>
+      <div>
+        <h2 className="text-2xl font-bold text-center">Shortened URL</h2>
+        <div className="flex items-center gap-4 p-4 bg-gray-500 rounded-lg">
+          <span className="text-lg font-mono">https://short.url/abc123</span>
+          <span className="px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-lg">
+            Shortned
+          </span>{" "}
+        </div>
+      </div>
     </div>
   );
 }
