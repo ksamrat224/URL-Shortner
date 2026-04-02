082dc713475d17f953e962c7540772b1d0defb72
Samrat <ksamrat224@gmail.com>
2026-04-02 21:15:40 +0545

    feat: add URL input state and dynamic display in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+import React, { useState } from "react";
+
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+
+  const handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black p-4">
       <h1 className="text-4xl font-bold text-center">URL Shortner</h1>
       <input
         type="text"
+        value={url}
+        onChange={handleUrlChange}
         className="w-full p-4 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         placeholder="Enter your url"
       />
@@ -13,10 +25,7 @@ export default function Home() {
       <div>
         <h2 className="text-2xl font-bold text-center">Shortened URL</h2>
         <div className="flex items-center gap-4 p-4 bg-gray-500 rounded-lg">
-          <span className="text-lg font-mono">https://short.url/abc123</span>
-          <span className="px-4 py-2 text-lg font-semibold text-white bg-blue-500 rounded-lg">
-            Shortned
-          </span>{" "}
+          <span className="text-lg font-mono">{url}</span>
         </div>
       </div>
     </div>
